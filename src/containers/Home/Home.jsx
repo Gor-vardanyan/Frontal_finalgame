492cbe8094ea65c14f04c2c6522a58e9a898c2f4
Gor <g.var.design@gmail.com>
2020-12-29 00:53:35 +0100

    header footer and home views concept

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import img from '../../images/street-fighter-.jpg'
+const Home = () => {
+    return(
+     <div className="home">
+         <img src={img} alt=""/>
+     </div>   
+    )
+}
+
+
+export default Home;
